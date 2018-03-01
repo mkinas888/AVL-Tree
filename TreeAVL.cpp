af6462f3516a5e9e93708714aca649700bdbb696
mkinas888 <mkinas888@gmail.com>
2018-03-01 19:55:00 +0100

    add all rotations

--- TreeAVL.cpp
+++ TreeAVL.cpp
@@ -13,12 +13,74 @@ void TreeAVL::setBf (NodeAVL *p) {
     p->bf = treeHeight(p->rightSibling) - treeHeight(p->leftSibling);
 }
 
+NodeAVL* TreeAVL::rotateLL (NodeAVL *p) {
+    NodeAVL *r = p->rightSibling;
+    r->parent = p->parent;
+    p->rightSibling = r->leftSibling;
+    if( p->rightSibling != NULL) {
+        p->rightSibling->parent = p;
+    }
+    r->leftSibling = p;
+    p->parent = r;
+    if (r->parent != NULL) {
+        if (r->parent->rightSibling == p) {
+            r->parent->rightSibling = r;
+        }
+        else {
+            r->parent->leftSibling = r;
+        }
+    }
+    setBf(r);
+    setBf(p);
+    return r;
+}
+
+NodeAVL* TreeAVL::rotateRR (NodeAVL *p) {
+    NodeAVL *r = p->leftSibling;
+    r->parent = p->parent;
+    p->leftSibling = r->rightSibling;
+    if (p->leftSibling != NULL) {
+        p->leftSibling->parent = p;    
+    }
+    r->rightSibling = p;
+    p->parent = r;
+    if (r->parent != NULL) {
+        if (r->parent->rightSibling == p) {
+            r->parent->rightSibling = r;
+        }
+        else {
+            r->parent->leftSibling = r;
+        }
+    }
+    setBf(r);
+    setBf(p);
+    return r;
+}
+
+NodeAVL* TreeAVL::rotateLR (NodeAVL *p) {
+    p->leftSibling = rotateLL(p->leftSibling);
+    return rotateRR(p);
+}
+
+NodeAVL* TreeAVL::rotateRL (NodeAVL *p) {
+    p->rightSibling = rotateRR(p->rightSibling);
+    return rotateLL(p);
+}
+
 void TreeAVL::restoreBalance (NodeAVL *p) {
     setBf(p);
     if (p->bf == 2) {
-        //TODO determine when rotate RR and when RL
+        if (p->rightSibling->bf == -1) {
+            rotateLR(p);
+        } else {
+            rotateLL(p);
+        }
     } else if (p->bf == -2) {
-        //TODO determine when rotate LL and when LR
+        if (p->leftSibling->bf == 1) {
+            rotateRL(p);
+        } else {
+            rotateRR(p);
+        }
     }
     if (p->parent != NULL) {
         restoreBalance(p->parent);
--- TreeAVL.hh
+++ TreeAVL.hh
@@ -8,10 +8,10 @@ class TreeAVL
 {
   public:
     NodeAVL *rootNode;
-    NodeAVL rotateRR(NodeAVL *p); // all kinds of rotations needed to rebalance node
-    NodeAVL rotateLL(NodeAVL *p); // when new one is added
-    NodeAVL rotateRL(NodeAVL *p);
-    NodeAVL rotateLR(NodeAVL *p);
+    NodeAVL* rotateRR(NodeAVL *p); // all kinds of rotations needed to rebalance node
+    NodeAVL* rotateLL(NodeAVL *p); // when new one is added
+    NodeAVL* rotateRL(NodeAVL *p);
+    NodeAVL* rotateLR(NodeAVL *p);
 
     int treeHeight(NodeAVL *p);
     void setBf(NodeAVL *p);          // setting balance factor when new node is added to tree
