1c4b5e5699710c19b620e760cac6aa43ee02483b
mkinas888 <mkinas888@gmail.com>
2018-05-11 23:43:00 +0200

    Delete binary files and vscode setting + add gitignore file

--- main.cpp
+++ main.cpp
@@ -27,19 +27,19 @@ int main() {
 
 
 
-    float Tk[100];   // tablica kluczy węzłów
+    float Tk[1000];   // tablica kluczy węzłów
     int i,x,i1,i2;
 
     srand(time(NULL));        // inicjujemy generator pseudolosowy
 
 
-    for(i = 0; i < 100; i++)   // Tablicę wypełniamy wartościami kluczy
+    for(i = 0; i < 1000; i++)   // Tablicę wypełniamy wartościami kluczy
         Tk[i] = i + 1;
     
-    for(i = 0; i < 200; i++)  // Mieszamy tablicę
+    for(i = 0; i < 2000; i++)  // Mieszamy tablicę
     {
-        i1 = rand() % 100;       // Losujemy 2 indeksy
-        i2 = rand() % 100;
+        i1 = rand() % 1000;       // Losujemy 2 indeksy
+        i2 = rand() % 1000;
 
         x = Tk[i1];             // Wymieniamy Tk[i1] <--> Tk[i2]
         Tk[i1] = Tk[i2];
@@ -52,7 +52,7 @@ int main() {
     high_resolution_clock::time_point t1 = high_resolution_clock::now();    // Get time before function
 
 
-    for(i = 0; i < 100; i++)                                               // Create AVL tree from array
+    for(i = 0; i < 1000; i++)                                               // Create AVL tree from array
     {
         tree.addAndRebalance(tree.rootNode,Tk[i]);
     }
@@ -67,6 +67,7 @@ int main() {
     tree.printBT("","",tree.rootNode);    // Print tree
     cout << duration << endl;
     
+    /*
     cout << endl << endl;          
     tree.inorderTravel(tree.rootNode);
     cout << endl << endl;
@@ -74,6 +75,6 @@ int main() {
     cout << endl << endl;
     tree.postorderTravel(tree.rootNode);
     cout << endl << endl <<  duration;
-
+    */
     
 }
